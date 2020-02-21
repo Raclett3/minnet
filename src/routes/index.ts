da649f02a5f882497c06c9a24a478dd873a88eaa
Raclett3 <im@s283.net>
2020-02-22 00:16:41 +0900

    Add a webfinger mock

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,9 @@
 import Router from '@koa/router';
 import Koa from 'koa';
+import mount from 'koa-mount';
 import send from 'koa-send';
+
+import wellKnown from './well-known';
 
 const router = new Router();
 const sendOptions = { root: process.cwd() + '/build/client/' };
@@ -11,5 +14,6 @@ router.get('/app.js', ctx => send(ctx, 'app.js', sendOptions));
 const app = new Koa();
 app.use(router.routes());
 app.use(router.allowedMethods());
+app.use(mount(wellKnown));
 
 export default app;
