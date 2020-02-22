f2d93d8939c131fa4a11547f8839e98bc44ae7c0
Raclett3 <im@s283.net>
2020-02-23 02:55:22 +0900

    Modify the loader to load the config before starting up

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import { loadConfig } from './config';
 import app from './server';
 
 (() => {
   console.log('Minnet v0.0.0');
+  loadConfig(process.cwd() + '/config/config.json');
   app.listen(3000);
 })();
