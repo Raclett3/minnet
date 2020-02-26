961e27ac518ef96d909dbcd5e0e81f0526970cba
Raclett3 <im@s283.net>
2020-02-26 12:53:10 +0900

    Add an account model

---
+++ src/controllers/accounts.ts
@@ -0,0 +1,68 @@
+import { getConnection } from 'typeorm';
+import { URL } from 'url';
+
+import { Account } from '../entities/account';
+import { generateId } from '../helpers/generate-id';
+import { ControllerError } from './error';
+
+const validateUsername = (username: string) => /^[a-zA-Z]{1,32}$/.test(username);
+
+function validateURI(uri: string, host: string) {
+  const validator = /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9-]*[A-Za-z0-9])$/;
+  const parsed = new URL(uri);
+  return validator.test(host) && parsed.host === host;
+}
+
+export async function createRemoteAccount(
+  username: string,
+  host: string,
+  name: string,
+  uri: string,
+  inbox: string,
+): Promise<void> {
+  if (!validateUsername(username)) {
+    throw new ControllerError('The username is invalid');
+  }
+
+  if (!validateURI(uri, host)) {
+    throw new ControllerError('The URI is invalid');
+  }
+
+  if (!validateURI(inbox, host)) {
+    throw new ControllerError('The inbot URI is invalid');
+  }
+
+  return await getConnection().transaction(async transaction => {
+    const exist = await transaction.findOne(Account, { username: username, host: host });
+    if (exist) {
+      throw new ControllerError('The username and host already exists');
+    }
+
+    const account = new Account(generateId(), username, host, name, uri, inbox);
+    await transaction.insert(Account, account);
+  });
+}
+
+export async function createLocalAccount(username: string, name: string): Promise<void> {
+  if (!validateUsername(username)) {
+    throw new ControllerError('The username is invalid');
+  }
+
+  return await getConnection().transaction(async transaction => {
+    const exist = await transaction.findOne(Account, { username: username });
+    if (exist) {
+      throw new ControllerError('The username already exists');
+    }
+
+    const account = new Account(generateId(), username, null, name, null, null);
+    await transaction.insert(Account, account);
+  });
+}
+
+export async function findAccount(username: string, host: string | null = null): Promise<Account | undefined> {
+  return await getConnection().transaction(async transaction => {
+    const account = await transaction.findOne(Account, { username: username, host: host });
+
+    return account;
+  });
+}
---
+++ src/entities/account.ts
@@ -0,0 +1,43 @@
+import { Column, Entity, Index, PrimaryColumn } from 'typeorm';
+
+// Some columns get null for local users
+
+@Entity()
+@Index(['username', 'host'], { unique: true })
+export class Account {
+  @PrimaryColumn('varchar', { length: 16 })
+  public id: string;
+
+  @Index()
+  @Column('varchar', { length: 128 })
+  public username: string;
+
+  @Column('varchar', { length: 128, nullable: true })
+  public host: string | null;
+
+  @Column('varchar', { length: 128 })
+  public name: string;
+
+  @Index({ unique: true })
+  @Column('varchar', { length: 512, nullable: true })
+  public uri: string | null;
+
+  @Column('varchar', { length: 512, nullable: true })
+  public inbox: string | null;
+
+  constructor(
+    id: string,
+    username: string,
+    host: string | null,
+    name: string,
+    uri: string | null,
+    inbox: string | null,
+  ) {
+    this.id = id;
+    this.username = username;
+    this.host = host;
+    this.name = name;
+    this.uri = uri;
+    this.inbox = inbox;
+  }
+}
