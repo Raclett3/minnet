36e031fe1b6eed7274c1ed3a310fa0a7ebd18b6c
Raclett3 <im@s283.net>
2020-02-26 13:06:39 +0900

    Add postgres connection initializer

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { loadConfig } from './config';
+import { initPostgres } from './postgres';
 import app from './server';
 
-(() => {
+(async () => {
   console.log('Minnet v0.0.0');
   loadConfig(process.cwd() + '/config/config.json');
+  await initPostgres();
   app.listen(3000);
 })();
---
+++ src/postgres.ts
@@ -0,0 +1,23 @@
+import { createConnection, getConnection } from 'typeorm';
+
+import { Account } from './entities/account';
+
+export function initPostgres() {
+  try {
+    return Promise.resolve(getConnection());
+  } catch {
+    const isTest = process.env.NODE_ENV === 'test';
+    return createConnection({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.MINNET_DB_USERNAME,
+      password: process.env.MINNET_DB_PASSWORD,
+      database: isTest ? 'minnet_test' : 'minnet',
+      synchronize: isTest,
+      dropSchema: isTest,
+      logging: false,
+      entities: [Account],
+    });
+  }
+}
