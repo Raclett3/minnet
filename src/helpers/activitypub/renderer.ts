172f39103075153bb3a015e2b4baf9fba0631400
Raclett3 <im@s283.net>
2020-03-10 17:01:53 +0900

    Add note renderer

--- src/helpers/activitypub/renderer.ts
+++ src/helpers/activitypub/renderer.ts
@@ -1,4 +1,5 @@
 import { configCache } from '../../config';
+import { generateId } from '../generate-id';
 
 export function appendContext(object: {}) {
   return {
@@ -24,5 +25,35 @@ export function renderLocalPerson(username: string, name: string, publicKeyPem: 
       owner: `https://${configCache.host}/users/${username}`,
       publicKeyPem: publicKeyPem,
     },
+  };
+}
+
+export function renderNote(id: string, date: Date, attributedTo: string, content: string, inReplyTo?: string) {
+  if (!configCache) {
+    throw Error('Configがロードされていません。');
+  }
+
+  return {
+    id: `https://${configCache.host}/notes/${id}`,
+    type: 'Note',
+    published: date.toISOString(),
+    attributedTo: attributedTo,
+    inReplyTo: inReplyTo,
+    content: content,
+    to: 'https://www.w3.org/ns/activitystreams#Public',
+  };
+}
+
+export function renderCreate(actor: string, object: {}) {
+  if (!configCache) {
+    throw Error('Configがロードされていません。');
+  }
+
+  return {
+    id: `https://${configCache.host}/create/${generateId()}`,
+    type: 'Create',
+    actor: actor,
+
+    object: object,
   };
 }
--- src/routes/api/notes/create-note.ts
+++ src/routes/api/notes/create-note.ts
@@ -2,6 +2,7 @@ import Koa from 'koa';
 import { URL } from 'url';
 
 import { configCache } from '../../../config';
+import { renderCreate, renderNote } from '../../../helpers/activitypub/renderer';
 import { deliver } from '../../../remote/deliver';
 
 export default async (ctx: Koa.Context) => {
@@ -27,25 +28,9 @@ export default async (ctx: Koa.Context) => {
   const url = new URL(inReplyTo);
   const date = new Date();
   const actor = `https://${configCache.host}/users/asahi`;
-  const id = Math.floor(Math.random() * 10000000);
+  const id = String(Math.floor(Math.random() * 10000000));
 
-  const document = {
-    '@context': 'https://www.w3.org/ns/activitystreams',
-
-    id: `https://${configCache.host}/create/${id}`,
-    type: 'Create',
-    actor: actor,
-
-    object: {
-      id: `https://${configCache.host}/notes/${id}`,
-      type: 'Note',
-      published: date.toISOString(),
-      attributedTo: actor,
-      inReplyTo: inReplyTo,
-      content: text,
-      to: 'https://www.w3.org/ns/activitystreams#Public',
-    },
-  };
+  const document = renderCreate(actor, renderNote(id, date, actor, text, inReplyTo));
 
   if (!deliver(actor, `${url.protocol}//${url.host}/inbox`, document)) {
     ctx.status = 500;
