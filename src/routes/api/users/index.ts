c962f13dd6328eafc974839b0c15205cc7c4ad55
Raclett3 <im@s283.net>
2020-03-12 22:02:06 +0900

    Add sign-in API endpoint

--- src/routes/api/users/index.ts
+++ src/routes/api/users/index.ts
@@ -1,9 +1,11 @@
 import Router from '@koa/router';
 
+import signIn from './signin';
 import signUp from './signup';
 
 const router = new Router();
 
 router.post('/', signUp);
+router.post('/signin', signIn);
 
 export default router;
