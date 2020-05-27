02442214067a57ae2f2b88bece6e95899ff1d8e6
Raclett3 <im@s283.net>
2020-05-27 20:07:18 +0900

    Add validateRemoteUsername()

--- src/helpers/validators.ts
+++ src/helpers/validators.ts
@@ -1,2 +1,3 @@
 export const validateUsername = (username: string) => /^[a-zA-Z0-9]{1,32}$/.test(username);
+export const validateRemoteUsername = (username: string) => /^[a-zA-Z0-9_-]+/.test(username);
 export const validatePassword = (password: string) => /^[\x20-\x7e]{8,64}$/.test(password);
