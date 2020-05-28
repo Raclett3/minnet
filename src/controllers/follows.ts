5e36cb292cc2aa4ec7a7ea15c47754696c470434
Raclett3 <im@s283.net>
2020-05-28 14:15:41 +0900

    Modify followerInboxes() to use Set()

--- src/controllers/follows.ts
+++ src/controllers/follows.ts
@@ -5,11 +5,8 @@ import { Follow } from '../entities/follow';
 export async function followerInboxes(followeeId: string) {
   const repository = getRepository(Follow);
   const followers = await repository.find({ followee: { id: followeeId } });
-  return followers.reduce<string[]>((acc, follow) => {
-    const inbox = follow.follower.inbox;
-    if (inbox && !acc.includes(inbox)) {
-      acc.push(inbox);
-    }
-    return acc;
-  }, []);
+  return followers.reduce<Set<string>>(
+    (acc, follow) => (follow.follower.inbox ? acc.add(follow.follower.inbox) : acc),
+    new Set(),
+  );
 }
