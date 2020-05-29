718af54d82f8c4a7bacea054addd814fab23d432
Raclett3 <im@s283.net>
2020-05-29 16:06:22 +0900

    Add /follow APIs

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -2,11 +2,13 @@ import Router from '@koa/router';
 import Koa from 'koa';
 import body from 'koa-body';
 
+import follow from './follow';
 import notes from './notes';
 import users from './users';
 
 const router = new Router();
 
+router.use('/api/follow', follow.routes(), follow.allowedMethods());
 router.use('/api/notes', notes.routes(), notes.allowedMethods());
 router.use('/api/users', users.routes(), users.allowedMethods());
 
