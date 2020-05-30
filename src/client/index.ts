9d854abf966ac65cae851f6df3157490ac89d303
Raclett3 <im@s283.net>
2020-05-30 11:04:15 +0900

    Add not-found page to router

--- src/client/index.ts
+++ src/client/index.ts
@@ -4,12 +4,16 @@ import Router from 'vue-router';
 import App from './App.vue';
 import './static/index.html';
 import Index from './views/Index.vue';
+import NotFound from './views/NotFound.vue';
 
 Vue.use(Router);
 
 const router = new Router({
   mode: 'history',
-  routes: [{ path: '/', component: Index }],
+  routes: [
+    { path: '/', component: Index },
+    { path: '*', component: NotFound },
+  ],
 });
 
 new Vue({
---
+++ src/client/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Not found</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class extends Vue {}
+</script>
+
+<style scoped></style>
