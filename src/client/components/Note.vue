53cf25a794fc03e7eb1f0946d687ab51990ea41c
Raclett3 <im@s283.net>
2020-05-31 16:21:59 +0900

    Add Note component

---
+++ src/client/components/Note.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="note">
+    <div class="author">{{ author }}</div>
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class extends Vue {
+  @Prop({ default: '' }) public author!: string;
+}
+</script>
+
+<style scoped>
+.note {
+  border-bottom: 1px solid #aaa;
+  color: #111;
+  padding: 10px;
+  width: calc(100% - 20px);
+}
+
+.author {
+  font-weight: bold;
+}
+</style>
