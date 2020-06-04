2dcfa978c36134c22f6d456b30be7fc72db0e6d0
Raclett3 <im@s283.net>
2020-06-04 13:50:54 +0900

    Add note posting page

--- src/client/views/Index.vue
+++ src/client/views/Index.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="timeline">
-    <note v-for="note in timeline" :key="note.id" :author="note.postedBy.name">{{ note.content }}</note>
+  <div>
+    <div class="post">
+      <m-textarea v-model="text" @enter="post()" />
+      <m-button @click="post()">Post</m-button>
+    </div>
+    <div class="timeline">
+      <note v-for="note in timeline" :key="note.id" :author="note.postedBy.name">{{ note.content }}</note>
+    </div>
   </div>
 </template>
 
@@ -8,12 +14,17 @@
 import { Component, Vue } from 'vue-property-decorator';
 
 import { APINote } from '../../routes/api/entities/note';
+import MButton from '../components/MButton.vue';
+import MTextarea from '../components/MTextarea.vue';
 import Note from '../components/Note.vue';
 import { api } from '../lib/request';
 
-@Component({ components: { Note } })
+@Component({ components: { MButton, MTextarea, Note } })
 export default class extends Vue {
   public timeline: APINote[] = [];
+  public text = '';
+  public token = localStorage.getItem('token');
+
   private ws!: WebSocket;
 
   public async getTimeline() {
@@ -29,10 +40,24 @@ export default class extends Vue {
     };
     this.getTimeline();
   }
+
+  public async post() {
+    if (this.token) {
+      await api('post', '/api/notes', { token: this.token, text: this.text });
+      this.text = '';
+    }
+  }
 }
 </script>
 
 <style scoped>
+.post {
+  background-color: rgba(32, 32, 32, 0.2);
+  margin: 0 auto;
+  width: 100%;
+  max-width: 768px;
+}
+
 .timeline {
   background-color: #fff;
   margin: 0 auto;
